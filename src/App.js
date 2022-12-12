6ebf6e341a892d7c00ab9de8dda118595c1bf485
puranchandralohar <clpuran95@gmail.com>
2022-12-12 20:32:16 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState} from 'react';
+import jwt_decode from 'jwt-decode'
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID Token" + response.credential);
+
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject)
+    setUser(userObject)
+
+  }
+
+  const handleSignout =(e)=>{
+    setUser({});
+  }
+
+  useEffect(()=>{
+    // global google
+    google.accounts.id.initialize({
+      client_id:"38111871990-vrga13euflrsjnnjclbkf56upg3dnpb7.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline" , size:"large"}
+    );
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='signInDiv'></div>
+      {
+        Object.keys(user).length !=0 && 
+      <button onClick={(e)=>handleSignout(e)}>Sign Out</button>
+      }
+      { user &&
+        <div>
+          <img src={user.picture}/>
+          <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }
